cdbcc9dd12583c828c506cf149bcf7cfd99f022e
alexvongmathep99-pixel <alexvongmathep99@gmail.com>
2026-06-01 09:28:18 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/my-event-budget-app/', // ⚠️ ຫ້າມລືມເຄື່ອງໝາຍ ຈຸດ (,) ທາງທ້າຍເດີ້
-})
