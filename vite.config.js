2160c36c24b7229768df6aa43c4b3dd00cb6f68b
alexvongmathep99-pixel <alexvongmathep99@gmail.com>
2026-06-09 14:20:51 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/my-event-budget-app/', // ⚠️ ຫ້າມລືມເຄື່ອງໝາຍ ຈຸດ (,) ທາງທ້າຍເດີ້
+  plugins: [react()],
+  base: '/my-event-budget-app/', // ⚠️ ຕ້ອງເພີ່ມແຖວນີ້ເຂົ້າໄປໃຫ້ຕົງກັບຊື່ Repository ເດີ້
 })
