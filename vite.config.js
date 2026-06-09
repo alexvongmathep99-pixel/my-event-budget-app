47a0ff249b475d0cb9e22f95751674c38754d826
alexvongmathep99-pixel <alexvongmathep99@gmail.com>
2026-06-09 14:42:18 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // ⚠️ ປ່ຽນມາໃຊ້ຕົວປົກກະຕິທີ່ເຄື່ອງເຈົ້າມີ
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/my-event-budget-app/', // ⚠️ ຄ່າເສັ້ນທາງສຳລັບ GitHub Pages
-})
